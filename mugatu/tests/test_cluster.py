46a11945a392344737f0ac77f237ec088b0b6a59
Joe <joegezo@gmail.com>
2021-07-04 13:14:55 -0400

    started integrating OPTICS

--- mugatu/tests/test_cluster.py
+++ mugatu/tests/test_cluster.py
@@ -7,8 +7,9 @@ Created on Sun May 30 13:52:46 2021
 """
 import numpy as np
 import pandas as pd
+import pytest
 
-from mugatu._cluster import reduce_and_cluster, compute_clusters
+from mugatu._cluster import reduce_and_cluster, compute_clusters, reduce_and_cluster_optics
 
 def test_reduce_and_cluster():
     N = 1000
@@ -61,14 +62,58 @@ def test_reduce_and_cluster_too_few_in_bin():
     assert isinstance(indices[0], np.ndarray)
     assert len(indices) == 4
     
+def test_reduce_and_cluster_optics():
+    N = 1000
+    min_samples = 5
+    test_data = np.random.normal(0, 1, (N, 20))
+    test_index = np.arange(N) + 13
+    indices = reduce_and_cluster_optics(test_data, test_index, 5, min_samples)
+    # check data types of index
+    assert isinstance(indices, list)
+    assert isinstance(indices[0], np.ndarray)
+    # check correct number of clusters
+    assert len(indices) >= 1 #== k
+    # check that all records accounted for
+    assert np.sum([len(x) for x in indices]) == N
     
     
-def test_compute_clusters():
+def test_reduce_and_cluster_optics_empty_bin():
+    min_samples = 5
+    test_data = np.random.normal(0,1,size=(0,10))
+    test_index = np.arange(0) 
+    indices = reduce_and_cluster_optics(test_data, test_index, 5, min_samples)
+    # check data types of index
+    assert isinstance(indices, list)
+    assert len(indices) == 0
+      
+    
+def test_compute_clusters_kmeans():
     N = 1000
     k = 5
     df = pd.DataFrame({"x":np.random.normal(0,1,N), "y":np.random.normal(0,1,N)}, index=np.arange(N)+N)
     cover = [np.arange(N,int(1.5*N)), np.arange(int(1.4*N), int(1.9*N))]
-    indices = compute_clusters(df, cover, False, k)
+    indices = compute_clusters(df, cover, pca_dim=False, k=k)
     assert len(indices) == k*len(cover)
     
+def test_compute_clusters_optics():
+    N = 1000
+    min_samples = 5
+    df = pd.DataFrame({"x":np.random.normal(0,1,N), "y":np.random.normal(0,1,N)}, index=np.arange(N)+N)
+    cover = [np.arange(N,int(1.5*N)), np.arange(int(1.4*N), int(1.9*N))]
+    indices = compute_clusters(df, cover, pca_dim=False, min_samples=min_samples)
+    assert len(indices) >= 1
+    
+def test_compute_clusters_missing_kwargs():
+    N = 1000
+    df = pd.DataFrame({"x":np.random.normal(0,1,N), "y":np.random.normal(0,1,N)}, index=np.arange(N)+N)
+    cover = [np.arange(N,int(1.5*N)), np.arange(int(1.4*N), int(1.9*N))]
+    with pytest.raises(UnboundLocalError):
+        indices = compute_clusters(df, cover)
+        
+def test_compute_clusters_conflicting_kwargs():
+    N = 1000
+    df = pd.DataFrame({"x":np.random.normal(0,1,N), "y":np.random.normal(0,1,N)}, index=np.arange(N)+N)
+    cover = [np.arange(N,int(1.5*N)), np.arange(int(1.4*N), int(1.9*N))]
+    with pytest.raises(UnboundLocalError):
+        indices = compute_clusters(df, cover, k=5, min_samples=10)
     
